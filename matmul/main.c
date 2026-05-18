a9d5657edad75d4d3a3d952981c98db65843301a
Michele Mesiti <michele.mesiti@kit.edu>
2026-05-19 01:43:16 +0200

    Minimal LIKWID marker instrumentation

--- matmul/main.c
+++ matmul/main.c
@@ -1,6 +1,6 @@
 #include "matmul_v1.c"
 #include "init.c"
-
+#include <likwid-marker.h>
 
 int main(){
 
@@ -14,10 +14,20 @@ int main(){
     // We want to compute D = Bt A B
     // C = A B
     // D = Bt C
+
+    LIKWID_MARKER_INIT;
+    LIKWID_MARKER_REGISTER("MM");
+    LIKWID_MARKER_REGISTER("MTM");
+
+    LIKWID_MARKER_START("MM");
     matmat_mul(a,b,c,N);
+    LIKWID_MARKER_STOP("MM");
+    LIKWID_MARKER_START("MTM");
     mat_t_mat_mul(b,c,d,N);
+    LIKWID_MARKER_STOP("MTM");
 
     free(d);free(c);free(b);free(a);
+    LIKWID_MARKER_CLOSE;
     return 0;
 
 }
